1b0b4f1849c29680e1285c5566846fabf49d8285
Psycho25-ux <bloggercom30.com>
2025-10-31 20:05:49 +0530

    Updated frontend url /

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['https://bookit-frontend-orpin.vercel.app/', 'http://localhost:5173'],
+  origin: ['https://bookit-frontend-orpin.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
